fc9924dfff8533b9e4917f572a3995580fdaf696
jadonylin <jadonlin2@gmail.com>
2025-03-06 20:20:37 +1100

    Add support for acceleration function to accept extra args
    Add and catch custom InterpolateError exception when angles go out of bounds during integration
    
    Record aberration angle and acceleration values during integration
    
    Clean up some lines of code

--- Dynamics/cmvint.py
+++ Dynamics/cmvint.py
@@ -1,4 +1,3 @@
-
 """
 cmvint - comoving integrator
 
@@ -10,41 +9,49 @@ import numpy as np
 
 import time
 
-from specrel import vadd, SinCosEpsilon, Lorentz
+from specrel import vadd, SinCosEpsilon, Lorentz, SinCosTheta
 
+class InterpolateError(ValueError):
+    pass
+
+def _func(func,tn,yn,vL,i,args):  
+    if args == ():
+        return func(tn,yn,vL,i)
+    else:
+        return func(tn,yn,vL,i,args)
 
 def Mstep(func,h,tn,yn,vL,i,args=()):
     """
     Step in the direction of the state-vector derivative, func, in frame Mn using fourth-order Runge-Kutta.
     """
 
-    k1 = h*func(tn      , yn       , vL, i)
-    k2 = h*func(tn+0.5*h, yn+0.5*k1, vL, i)
-    k3 = h*func(tn+0.5*h, yn+0.5*k2, vL, i)
-    k4 = h*func(tn+h    , yn+k3    , vL, i)
+    k1 = h*_func(func, tn      , yn       , vL, i, args)
+    k2 = h*_func(func, tn+0.5*h, yn+0.5*k1, vL, i, args)
+    k3 = h*_func(func, tn+0.5*h, yn+0.5*k2, vL, i, args)
+    k4 = h*_func(func, tn+h    , yn+k3    , vL, i, args)
 
     yNew = yn + 1/6*(k1 + 2*k2 + 2*k3 + k4)
     tNew = tn + h
+    ydotNew = k1/h
 
-    return tNew,yNew
+    return tNew,yNew,ydotNew
 
-def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
+def odecmvint(func, state0, t_max, v_max, args: tuple=(), hstep: float=1e-4):
     """
     Integrate func over time by passing between the comoving reference frame (frame M) and
     the light source reference frame (frame L).
 
-
     Parameters
     ----------
-    func  :   callable(t,y,vL,i,args). Computes the derivative of M-frame-state-vector, y, at time t
-              measured in the comoving M-frame.
-              vL is the velocity of the object in frame L, needed for Lorentz transformation. 
-              The integration step (i) is used for debugging.
-    y0    :   Array of initial conditions on y ([x, y, phi, vx, vy, vphi])
-    t_max :   Maximum integration runtime (seconds)
-    v_max :   Maximum object velocity before stopping integration (metres/second)
-    args  :   Extra arguments to pass to func
-    hstep :   Integration step size
+    func   :   callable(t,y,vL,i,args). Computes the derivative of M-frame-state-vector, y, at time t
+                measured in the comoving M-frame.
+                vL is the velocity of the object in frame L, needed for Lorentz transformation. 
+                The integration step (i) is used for debugging.
+    state0 :   Array of initial conditions for the state vector ([x, y, phi, vx, vy, vphi])
+    t_max  :   Maximum integration runtime (seconds)
+    v_max  :   Maximum object velocity before stopping integration (metres/second)
+    args   :   Extra arguments to pass to func
+    hstep  :   Integration step size
     
     Returns
     -------
@@ -62,11 +69,7 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
     """
 
     timeLn = 0  # starting time
-    x0, y0, phi0, vx0, vy0, omega0 = y0
-
-    time_MAX = t_max
-    vFINAL = v_max
-    h = hstep   # Step size  
+    x0, y0, phi0, vx0, vy0, omega0 = state0
 
     x_array = []
     y_array = []
@@ -85,6 +88,12 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
     eps_array = []
     eps_rate_array = []
 
+    # Aberration angle
+    theta_array = []
+
+    # Accelerations
+    accels = []
+
     # Flag if the optimisation took too long
     STOPPED = False
 
@@ -98,9 +107,9 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
 
     # Initial state vectors
     YMn = np.array([x0M, y0M, phi0, 0, 0, omega0])            
-    YL0 = np.array([x0, y0, vx0, vy0])       
     taun = 0
 
+    _, _, _, ax0, ay0, aphi0 = _func(func, timeMn, YMn, vn, 0, args)
 
     x_array.append(x0)
     y_array.append(y0)
@@ -113,14 +122,18 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
     eps_array.append(0.)
     eps_rate_array.append(0.)
 
+    theta_array.append(0.)  # At non-relativistic initial velocities, the aberration is negligible, set to 0
+
+    accels.append([ax0,ay0,aphi0])
+
     timeM_array.append(timeMn)
     tau_array.append(taun)
     timeL_array.append(timeLn)
 
     timeSTART = time.time()
+    
     i = 1
-
-    while (vn[0] < vFINAL):
+    while (vn[0] < v_max):
         """
         The main loop of the comoving integrator. Information is passed between frames L, Mn and Mn+1.
 
@@ -131,11 +144,13 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
             (in L) and the new small velocity due to M-frame forces (over the time step Delta tau).
         
             Rotation information is transferred from M_n to M_{n+1} purely via Wigner rotation.
+        
+        TODO: tidy up using fewer arrays
         """
         
         timeDIFF = time.time() - timeSTART
         
-        if timeDIFF >= time_MAX: # Finished
+        if timeDIFF >= t_max: # Finished
             STOPPED = True
             print("Integration time limit reached.")
             break
@@ -144,11 +159,14 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
             break    
         else:                                  
             # Take a step in M and evolve state there
+            
             try:
-                timeMNew, YNew = Mstep(func,h,timeMn,YMn,vn,i,args) 
-            except:  # TODO: catch specific exception
+                timeMNew, YNew, YdotNew = Mstep(func,hstep,timeMn,YMn,vn,i,args) 
+            except InterpolateError as ie:
                 STOPPED = True
-                print("Derivative vector evaluation failed. Successfully stopped early")
+                print("\n")
+                print(str(ie))
+                print("odecmvint: M-frame step failed. Successfully stopped early.")
                 break
 
             # Store new M
@@ -168,11 +186,12 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
             vLNew   = vadd(vn,uNew)  # Uses relativistic velocity addition
             zM_NEXT = Lorentz(vLNew,zLNew)  # Won't be at the origin anymore due to forces
             vM_NEXT = np.array([0,0])  # New velocity is 0 since we've just boosted into the new frame 
+            _, _, theta   = SinCosTheta(vLNew)
             eps     = SinCosEpsilon(vn,uNew)[2]  # Wigner rotation angle
             if i==1:
-                eps_rate = (eps - 0)/h
+                eps_rate = (eps - 0)/hstep
             else:    
-                eps_rate = (eps - eps_array[i-2])/h
+                eps_rate = (eps - eps_array[i-2])/hstep
             
 
             # Updating state vector and M-frame velocity
@@ -180,8 +199,8 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
             xM2     = zM_NEXT[1]
             yM2     = zM_NEXT[2]
             phiM2   = phiNew - eps  
-            vxM2    = 0
-            vyM2    = 0
+            vxM2    = vM_NEXT[0]
+            vyM2    = vM_NEXT[1]
             omegaM2 = omegaNew - eps_rate
 
             YMn = np.array([xM2,yM2,phiM2,vxM2,vyM2,omegaM2])
@@ -197,20 +216,24 @@ def odecmvint(func, y0, t_max, v_max, args=(), hstep=1e-4):
 
             # Saving M data
             timeM_array.append(timeMn)
-            tau_array.append(tau_array[i-1] + h)
+            tau_array.append(tau_array[i-1] + hstep)
             
             phi_array.append(phiM2)
             omega_array.append(omegaM2)
             
             eps_array.append(eps)
             eps_rate_array.append(eps_rate)
+
+            theta_array.append(theta)
+
+            accels.append([*YdotNew[3:]])
         
-        iFINAL = i
         i += 1
 
     positions = np.array([x_array, y_array, vx_array, vy_array])  # Frame L
-    angles    = np.array([phi_array, eps_array, omega_array, eps_rate_array])  # Frame M
+    angles    = np.array([phi_array, eps_array, omega_array, eps_rate_array, theta_array])  # Frame M
     times     = np.array([timeM_array,tau_array,timeL_array])
+    accels    = np.array(accels)
     loop_data = {'Runtime': round(timeDIFF), 'Steps': i, 'Stopped': STOPPED}
     
-    return positions, angles, times, loop_data
+    return positions, angles, times, accels, loop_data
