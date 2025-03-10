7127bff9fa51a10b7eaddbc0bcf79f29ef5d5300
jadonylin <jadonlin2@gmail.com>
2025-03-10 17:16:56 +1100

    Decompose odecmvint into subfunctions for readability and testing
    Add type annotations to functions

--- Dynamics/cmvint.py
+++ Dynamics/cmvint.py
@@ -14,27 +14,89 @@ from specrel import vadd, SinCosEpsilon, Lorentz, SinCosTheta
 class InterpolateError(ValueError):
     pass
 
-def _func(func,tn,yn,vL,i,args):  
+
+def create_coordinate_arrays():
+    x_array = []
+    y_array = []
+    vx_array = []
+    vy_array = []
+
+    timeM_array = []
+    tau_array = []
+    timeL_array = []
+
+    # Angles are stored in frame M
+    phi_array = []
+    omega_array = []
+
+    # Frame Wigner-rotation angle
+    eps_array = []
+    eps_rate_array = []
+
+    # Aberration angle
+    theta_array = []
+
+    # Accelerations
+    accels = []
+
+    return [x_array, y_array, vx_array, vy_array, 
+            timeM_array, tau_array, timeL_array, 
+            phi_array, omega_array, eps_array, eps_rate_array, theta_array,
+            accels]
+
+def append_coordinate_arrays(coordinate_arrays: list[list], items: list[float]):
+    """Append items to coordinate arrays element by element"""
+    for item, arr in zip(items,coordinate_arrays):
+        arr.append(item)
+    return coordinate_arrays
+
+def _func(func, tn, yn, vL, i, args):  
     if args == ():
         return func(tn,yn,vL,i)
     else:
         return func(tn,yn,vL,i,args)
 
-def Mstep(func,h,tn,yn,vL,i,args=()):
+def Mstep(func: callable, h: float, tn: float, yn: np.ndarray, vL: np.ndarray, i: int, args: tuple=()):
     """
     Step in the direction of the state-vector derivative, func, in frame Mn using fourth-order Runge-Kutta.
     """
-
     k1 = h*_func(func, tn      , yn       , vL, i, args)
     k2 = h*_func(func, tn+0.5*h, yn+0.5*k1, vL, i, args)
     k3 = h*_func(func, tn+0.5*h, yn+0.5*k2, vL, i, args)
     k4 = h*_func(func, tn+h    , yn+k3    , vL, i, args)
-
     yNew = yn + 1/6*(k1 + 2*k2 + 2*k3 + k4)
     tNew = tn + h
     ydotNew = k1/h
-
     return tNew,yNew,ydotNew
+
+def update_frame_L(timeMNew, YNew: np.ndarray, vn: np.ndarray):
+    """
+    Convert YNew (M-frame state vector) to frame L using inverse Lorentz transformation 
+    with new velocity vn.
+    """
+    xNew, yNew, phiNew, uxNew, uyNew, omegaNew = YNew
+    zNew  = np.array([timeMNew,xNew,yNew]) 
+    uNew  = np.array([uxNew,uyNew])  # Velocity induced by the M-frame forces over small time step
+    zLNew = Lorentz(-vn,zNew)
+    return zLNew, uNew
+
+def create_frame_MNext(zLNew: np.ndarray, uNew: np.ndarray, vn: np.ndarray):
+    # Defining new M_{n+1} frame as a boost from L
+    vLNew   = vadd(vn,uNew)  # Uses relativistic velocity addition
+    zM_NEXT = Lorentz(vLNew,zLNew)  # Won't be at the origin anymore due to forces
+    vM_NEXT = np.array([0,0])  # New velocity is 0 since we've just boosted into the new frame 
+    thetan  = SinCosTheta(vLNew)[2]
+    epsn    = SinCosEpsilon(vn,uNew)[2]  # Wigner rotation angle
+    return vLNew, zM_NEXT, vM_NEXT, thetan, epsn
+
+def update_frame_M_state(zM_NEXT: np.ndarray, vM_NEXT: np.ndarray, phiNew: float, omegaNew: float, epsn: float, eps_raten: float):
+    # Updating state vector and M-frame velocity
+    timeMn, xM2, yM2 = zM_NEXT
+    phiM2            = phiNew - epsn  
+    vxM2, vyM2       = vM_NEXT
+    omegaM2          = omegaNew - eps_raten
+    YMn = np.array([xM2,yM2,phiM2,vxM2,vyM2,omegaM2])
+    return timeMn, YMn
 
 def odecmvint(func, state0, t_max, v_max, args: tuple=(), hstep: float=1e-4):
     """
@@ -67,68 +129,40 @@ def odecmvint(func, state0, t_max, v_max, args: tuple=(), hstep: float=1e-4):
                     "Stopped" - bool, if the integrator ran out of time or encountered a force-calculation 
                     error
     """
-
-    timeLn = 0  # starting time
-    x0, y0, phi0, vx0, vy0, omega0 = state0
-
-    x_array = []
-    y_array = []
-    vx_array = []
-    vy_array = []
-
-    timeM_array = []
-    tau_array = []
-    timeL_array = []
-
-    # Angles are stored in frame M
-    phi_array = []
-    omega_array = []
-
-    # Frame Wigner-rotation angle
-    eps_array = []
-    eps_rate_array = []
-
-    # Aberration angle
-    theta_array = []
-
-    # Accelerations
-    accels = []
-
-    # Flag if the optimisation took too long
+    
+    # Flag for if the optimisation took too long or an integration step failed
     STOPPED = False
 
+    coordinate_arrays = create_coordinate_arrays()
+    x_array, y_array, vx_array, vy_array, timeM_array, tau_array, timeL_array,\
+    phi_array, omega_array, eps_array, eps_rate_array, theta_array,\
+    accels = coordinate_arrays
 
+    timeLn = 0.  # starting time
+    x0, y0, phi0, vx0, vy0, omega0 = state0
     vn = np.array([vx0, vy0])
-    z0 = np.array([timeLn, x0, y0])  # Four-position of the object (without the third spatial component)           
+
+    # Initial four-position in frame L (without the third spatial component)
+    z0 = np.array([timeLn, x0, y0]) 
 
     # Initial four-position in frame M
-    zM0    = Lorentz(vn,z0)
+    zM0 = Lorentz(vn,z0)
     timeMn, x0M, y0M = zM0
 
     # Initial state vectors
     YMn = np.array([x0M, y0M, phi0, 0, 0, omega0])            
     taun = 0
 
+    # At non-relativistic initial velocities, the correction angles are negligible, set to 0
+    epsn = 0.
+    eps_raten = 0.
+    thetan = 0.  
+    
     _, _, _, ax0, ay0, aphi0 = _func(func, timeMn, YMn, vn, 0, args)
+    acceln = [ax0,ay0,aphi0]
 
-    x_array.append(x0)
-    y_array.append(y0)
-    vx_array.append(vx0)
-    vy_array.append(vy0)
-
-    phi_array.append(phi0)
-    omega_array.append(omega0)
-
-    eps_array.append(0.)
-    eps_rate_array.append(0.)
-
-    theta_array.append(0.)  # At non-relativistic initial velocities, the aberration is negligible, set to 0
-
-    accels.append([ax0,ay0,aphi0])
-
-    timeM_array.append(timeMn)
-    tau_array.append(taun)
-    timeL_array.append(timeLn)
+    coordinates = [x0, y0, vx0, vy0, timeMn, taun, timeLn, phi0, omega0, epsn, eps_raten, thetan, acceln]
+    coordinate_arrays = append_coordinate_arrays(coordinate_arrays, coordinates)
 
     timeSTART = time.time()
     
@@ -144,89 +178,44 @@ def odecmvint(func, state0, t_max, v_max, args: tuple=(), hstep: float=1e-4):
             (in L) and the new small velocity due to M-frame forces (over the time step Delta tau).
         
             Rotation information is transferred from M_n to M_{n+1} purely via Wigner rotation.
-        
-        TODO: tidy up using fewer arrays
         """
         
         timeDIFF = time.time() - timeSTART
         
-        if timeDIFF >= t_max: # Finished
+        if timeDIFF >= t_max:
             STOPPED = True
             print("Integration time limit reached.")
-            break
         
         if STOPPED:
             break    
         else:                                  
-            # Take a step in M and evolve state there
-            
             try:
                 timeMNew, YNew, YdotNew = Mstep(func,hstep,timeMn,YMn,vn,i,args) 
             except InterpolateError as ie:
                 STOPPED = True
-                print("\n")
-                print(str(ie))
-                print("odecmvint: M-frame step failed. Successfully stopped early.")
+                print("\n" + str(ie) + "odecmvint: M-frame step failed. Successfully stopped early.")
                 break
-
-            # Store new M
-            xNew     = YNew[0]
-            yNew     = YNew[1]
-            phiNew   = YNew[2]
-            uxNew    = YNew[3]  
-            uyNew    = YNew[4]
+            
+            phiNew = YNew[2]
             omegaNew = YNew[5]
-
-            # Convert time and position variables to frame L using inverse Lorentz transformation
-            zNew  = np.array([timeMNew,xNew,yNew]) 
-            uNew  = np.array([uxNew,uyNew])  # Velocity induced by the M-frame forces over small time step
-            zLNew = Lorentz(-vn,zNew)
-
-            # Defining new M_{n+1} frame as a boost from L
-            vLNew   = vadd(vn,uNew)  # Uses relativistic velocity addition
-            zM_NEXT = Lorentz(vLNew,zLNew)  # Won't be at the origin anymore due to forces
-            vM_NEXT = np.array([0,0])  # New velocity is 0 since we've just boosted into the new frame 
-            _, _, theta   = SinCosTheta(vLNew)
-            eps     = SinCosEpsilon(vn,uNew)[2]  # Wigner rotation angle
+            zLNew, uNew = update_frame_L(timeMNew, YNew, vn)
+            vLNew, zM_NEXT, vM_NEXT, thetan, epsn = create_frame_MNext(zLNew, uNew, vn)
             if i==1:
-                eps_rate = (eps - 0)/hstep
+                eps_raten = (epsn - 0)/hstep
             else:    
-                eps_rate = (eps - eps_array[i-2])/hstep
+                eps_raten = (epsn - eps_array[i-2])/(2*hstep)
+            timeMn, YMn = update_frame_M_state(zM_NEXT, vM_NEXT, phiNew, omegaNew, epsn, eps_raten)
             
+            phiM2 = YMn[2]
+            omegaM2 = YMn[5]
 
-            # Updating state vector and M-frame velocity
-            timeMn  = zM_NEXT[0]
-            xM2     = zM_NEXT[1]
-            yM2     = zM_NEXT[2]
-            phiM2   = phiNew - eps  
-            vxM2    = vM_NEXT[0]
-            vyM2    = vM_NEXT[1]
-            omegaM2 = omegaNew - eps_rate
-
-            YMn = np.array([xM2,yM2,phiM2,vxM2,vyM2,omegaM2])
             vn = vLNew
+            taun = taun + hstep
 
-
-            # Saving L data
-            timeL_array.append(zLNew[0])
-            x_array.append(zLNew[1])
-            y_array.append(zLNew[2])
-            vx_array.append(vLNew[0])
-            vy_array.append(vLNew[1])
-
-            # Saving M data
-            timeM_array.append(timeMn)
-            tau_array.append(tau_array[i-1] + hstep)
-            
-            phi_array.append(phiM2)
-            omega_array.append(omegaM2)
-            
-            eps_array.append(eps)
-            eps_rate_array.append(eps_rate)
-
-            theta_array.append(theta)
-
-            accels.append([*YdotNew[3:]])
+            acceln = [*YdotNew[3:]]
+            coordinates = [zLNew[1], zLNew[2], *vLNew, 
+                            timeMn, taun, zLNew[0], phiM2, omegaM2, epsn, eps_raten, thetan, acceln]
+            append_coordinate_arrays(coordinate_arrays, coordinates)
         
         i += 1
 
