0bc911126090997ea895baae7efd156a8d40834c
jadonylin <jadonlin2@gmail.com>
2025-03-11 15:47:28 +1100

    Add finite difference function to calculate wigner time derivative
    Update documentation and type hints

--- Dynamics/cmvint.py
+++ Dynamics/cmvint.py
@@ -60,16 +60,16 @@ def Mstep(func: callable, h: float, tn: float, yn: np.ndarray, vL: np.ndarray, i
     """
     Step in the direction of the state-vector derivative, func, in frame Mn using fourth-order Runge-Kutta.
     """
-    k1 = h*_func(func, tn      , yn       , vL, i, args)
-    k2 = h*_func(func, tn+0.5*h, yn+0.5*k1, vL, i, args)
-    k3 = h*_func(func, tn+0.5*h, yn+0.5*k2, vL, i, args)
-    k4 = h*_func(func, tn+h    , yn+k3    , vL, i, args)
-    yNew = yn + 1/6*(k1 + 2*k2 + 2*k3 + k4)
+    k1 = _func(func, tn    , yn     , vL, i, args)
+    k2 = _func(func, tn+h/2, yn+k1/2, vL, i, args)
+    k3 = _func(func, tn+h/2, yn+k2/2, vL, i, args)
+    k4 = _func(func, tn+h  , yn+k3  , vL, i, args)
+    yNew = yn + h/6*(k1 + 2*k2 + 2*k3 + k4)
     tNew = tn + h
     ydotNew = k1/h
     return tNew,yNew,ydotNew
 
-def update_frame_L(timeMNew, YNew: np.ndarray, vn: np.ndarray):
+def update_frame_L(timeMNew: float, YNew: np.ndarray, vn: np.ndarray):
     """
     Convert YNew (M-frame state vector) to frame L using inverse Lorentz transformation 
     with new velocity vn.
@@ -81,7 +81,7 @@ def update_frame_L(timeMNew, YNew: np.ndarray, vn: np.ndarray):
     return zLNew, uNew
 
 def create_frame_MNext(zLNew: np.ndarray, uNew: np.ndarray, vn: np.ndarray):
-    # Defining new M_{n+1} frame as a boost from L
+    """Create M frame at the next step (M_{n+1}) via a boost from frame L"""
     vLNew   = vadd(vn,uNew)  # Uses relativistic velocity addition
     zM_NEXT = Lorentz(vLNew,zLNew)  # Won't be at the origin anymore due to forces
     vM_NEXT = np.array([0,0])  # New velocity is 0 since we've just boosted into the new frame 
@@ -90,7 +90,7 @@ def create_frame_MNext(zLNew: np.ndarray, uNew: np.ndarray, vn: np.ndarray):
     return vLNew, zM_NEXT, vM_NEXT, thetan, epsn
 
 def update_frame_M_state(zM_NEXT: np.ndarray, vM_NEXT: np.ndarray, phiNew: float, omegaNew: float, epsn: float, eps_raten: float):
-    # Updating state vector and M-frame velocity
+    """Update M-frame state vector including Wigner rotation angles"""
     timeMn, xM2, yM2 = zM_NEXT
     phiM2            = phiNew - epsn  
     vxM2, vyM2       = vM_NEXT
@@ -98,7 +98,15 @@ def update_frame_M_state(zM_NEXT: np.ndarray, vM_NEXT: np.ndarray, phiNew: float
     YMn = np.array([xM2,yM2,phiM2,vxM2,vyM2,omegaM2])
     return timeMn, YMn
 
-def odecmvint(func, state0, t_max, v_max, args: tuple=(), hstep: float=1e-4):
+def update_finite_difference(current_value: float, all_values: list, iteration: int, hstep: float):
+    """Calculate finite difference on an updating array"""
+    if iteration == 1:
+        derivative = (current_value - 0)/hstep
+    else:    
+        derivative = (current_value - all_values[iteration-2])/(2*hstep)
+    return derivative
+
+def odecmvint(func: callable, state0: np.ndarray, t_max: float, v_max: float, args: tuple=(), hstep: float=1e-4):
     """
     Integrate func over time by passing between the comoving reference frame (frame M) and
     the light source reference frame (frame L).
@@ -108,8 +116,8 @@ def odecmvint(func, state0, t_max, v_max, args: tuple=(), hstep: float=1e-4):
     func   :   callable(t,y,vL,i,args). Computes the derivative of M-frame-state-vector, y, at time t
                 measured in the comoving M-frame.
                 vL is the velocity of the object in frame L, needed for Lorentz transformation. 
-                The integration step (i) is used for debugging.
-    state0 :   Array of initial conditions for the state vector ([x, y, phi, vx, vy, vphi])
+                The integration step, i, is used for debugging.
+    state0 :   Initial conditions for the state vector ([x, y, phi, vx, vy, vphi])
     t_max  :   Maximum integration runtime (seconds)
     v_max  :   Maximum object velocity before stopping integration (metres/second)
     args   :   Extra arguments to pass to func
@@ -149,16 +157,17 @@ def odecmvint(func, state0, t_max, v_max, args: tuple=(), hstep: float=1e-4):
     zM0 = Lorentz(vn,z0)
     timeMn, x0M, y0M = zM0
 
-    # Initial state vectors
-    YMn = np.array([x0M, y0M, phi0, 0, 0, omega0])            
-    taun = 0
+    # Initial state vector
+    YMn = np.array([x0M, y0M, phi0, 0., 0., omega0])            
+    taun = 0.  # Simulation time/proper time of the comoving sail
 
     # At non-relativistic initial velocities, the correction angles are negligible, set to 0
     epsn = 0.
     eps_raten = 0.
     thetan = 0.  
     
-    _, _, _, ax0, ay0, aphi0 = _func(func, timeMn, YMn, vn, 0, args)
+    i = 0  # Integrator iteration index
+    _, _, _, ax0, ay0, aphi0 = _func(func, timeMn, YMn, vn, i, args)
     acceln = [ax0,ay0,aphi0]
 
     coordinates = [x0, y0, vx0, vy0, timeMn, taun, timeLn, phi0, omega0, epsn, eps_raten, thetan, acceln]
@@ -193,25 +202,24 @@ def odecmvint(func, state0, t_max, v_max, args: tuple=(), hstep: float=1e-4):
                 timeMNew, YNew, YdotNew = Mstep(func,hstep,timeMn,YMn,vn,i,args) 
             except InterpolateError as ie:
                 STOPPED = True
-                print("\n" + str(ie) + "odecmvint: M-frame step failed. Successfully stopped early.")
+                print("\n" + str(ie) + "\nodecmvint: M-frame step failed. Successfully stopped early.")
                 break
+            
+            zLNew, uNew = update_frame_L(timeMNew, YNew, vn)
+            vLNew, zM_NEXT, vM_NEXT, thetan, epsn = create_frame_MNext(zLNew, uNew, vn)
             
             phiNew = YNew[2]
             omegaNew = YNew[5]
-            zLNew, uNew = update_frame_L(timeMNew, YNew, vn)
-            vLNew, zM_NEXT, vM_NEXT, thetan, epsn = create_frame_MNext(zLNew, uNew, vn)
-            if i==1:
-                eps_raten = (epsn - 0)/hstep
-            else:    
-                eps_raten = (epsn - eps_array[i-2])/(2*hstep)
+            eps_raten = update_finite_difference(epsn, eps_array, i, hstep)
             timeMn, YMn = update_frame_M_state(zM_NEXT, vM_NEXT, phiNew, omegaNew, epsn, eps_raten)
-            
-            phiM2 = YMn[2]
-            omegaM2 = YMn[5]
 
             vn = vLNew
             taun = taun + hstep
 
+            # TODO: find a better way to store and return arrays that doesn't require explicitly extracting coordinates.
+            #       This should also simplify the number of subfunction arguments.
+            phiM2 = YMn[2]
+            omegaM2 = YMn[5]
             acceln = [*YdotNew[3:]]
             coordinates = [zLNew[1], zLNew[2], *vLNew, 
                             timeMn, taun, zLNew[0], phiM2, omegaM2, epsn, eps_raten, thetan, acceln]
