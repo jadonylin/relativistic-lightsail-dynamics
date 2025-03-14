66f32cbe5b613ae3c3134c03911a59eaadc24c56
jadonylin <jadonlin2@gmail.com>
2025-03-14 13:30:53 +1100

    Plot linear stability results for LvRFinalOpt

--- Dynamics/Dynamics_integrator.py
+++ Dynamics/Dynamics_integrator.py
@@ -23,32 +23,33 @@ wavelength = 1
 # The efficiency factors are too expensive to calculate in real time, so pre-calculated tables are used.
 klambda = 1000
 kdelta = 1000
-runID_load = "LvRFinalOpt"
-lookup_data_fname = rf'./Data/{runID_load}_Lookup_table_lambda_{klambda}_by_delta_{kdelta}.pkl'
+runID_load = "LvRFinalOpt_lsa"
+# lookup_data_fname = rf'./Data/{runID_load}_Lookup_table_lambda_{klambda}_by_delta_{kdelta}.pkl'
+lookup_data_fname = rf'./Data/{runID_load}_Lookup_table_lambda_{klambda}.pkl'
 opt_gratings_data_fname = './Data/FOM_optimisation_maxfev160000.pkl'
 
 w, lookup_data = forces.load_essential_data(opt_gratings_data_fname, 0,lookup_data_fname)
-interpolation_funcs = forces.create_interpolation_funcs(lookup_data)
-accel = forces.aM  # Choose time derivative state vector function
+interpolation_funcs = forces.create_lsa_interpolation_funcs(lookup_data)
+accel = forces.aM_linear  # Choose time derivative state vector function
 accel_args = (w, interpolation_funcs)  # Passed to accel
 
 ## Optimisation parameters and initial conditions ##
 x0 = 0
 vx0 = 0
 
-# ## Optimised - 1st
-# y0      = 0.05*w
-# phi0    = 1*np.pi/180
-# vy0     = -1.
-# omega0  = -1.
+## Optimised - 1st
+y0      = 0.05*w
+phi0    = 1*np.pi/180
+vy0     = -1.
+omega0  = -1.
 
-# Observing divergences
-x0 = 215177197.96531236
-y0 = -0.09459471262152874
-phi0 = -0.0027986233605225125
-vx0 = 2658365.0469323145
-vy0 = -11.247767769881074
-omega0 = 0.9393570477723936
+# # Observing divergences
+# x0 = 215177197.96531236
+# y0 = -0.09459471262152874
+# phi0 = -0.0027986233605225125
+# vx0 = 2658365.0469323145
+# vy0 = -11.247767769881074
+# omega0 = 0.9393570477723936
 
 ## Ilic - 1st
 # y0      = 1.3183489420398592e-07
@@ -76,7 +77,7 @@ time_MAX = 8.5*60*60  # Maximum runtime (seconds)
 velocity_MAX = 0.05*c
  
 h = 1e-4   # Step size  
-runID = "LvRFinalOpt_forces_module_test"  # Added to the output data filename
+runID = "LvRFinalOpt_lsa"  # Added to the output data filename
 
 positions, angles, times, accels, loop_data = odecmvint(accel, Y0, time_MAX, velocity_MAX, args=accel_args, hstep=h)
 
