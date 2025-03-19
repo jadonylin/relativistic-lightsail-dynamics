e6290216ce0dc1f87cdb0dd02b875fe2f71b50c1
jadonylin <jadonlin2@gmail.com>
2025-03-20 09:03:46 +1100

    Add initial attempts to calculate Jacobian-linearised dynamics for MdSnpmin grating

--- Dynamics/Dynamics_integrator.py
+++ Dynamics/Dynamics_integrator.py
@@ -23,33 +23,34 @@ wavelength = 1
 # The efficiency factors are too expensive to calculate in real time, so pre-calculated tables are used.
 klambda = 1000
 kdelta = 1000
-runID_load = "LvRFinalOpt"
-lookup_data_fname = f'./Data/{runID_load}_Lookup_table_lambda_{klambda}_by_delta_{kdelta}.pkl'
-# lookup_data_fname = f'./Data/{runID_load}_Lookup_table_lambda_{klambda}.pkl'
-opt_gratings_data_fname = f'./Data/{runID_load}_FOM_optimisation_maxfev160000.pkl'
+runID_load = "MdSnpminOpt_lsa"
+# lookup_data_fname = f'./Data/{runID_load}_Lookup_table_lambda_{klambda}_by_delta_{kdelta}.pkl'
+lookup_data_fname = f'./Data/{runID_load}_Lookup_table_lambda_{klambda}.pkl'
+# opt_gratings_data_fname = f'./Data/{runID_load}_FOM_optimisation_maxfev160000.pkl'
+opt_gratings_data_fname = f'./Data/FOM_optimisation_maxfev9000'
+num_processes = 18  # number of processes used in the optimisation to produce opt_gratings_data_fname.pkl
+output_opt_idx = 0  # Index of the optimum grating to output, lower corresponding to larger FOM
 
-w, lookup_data = forces.load_essential_data(opt_gratings_data_fname, 0,lookup_data_fname)
-interpolation_funcs = forces.create_interpolation_funcs(lookup_data)
-accel = forces.aM  # Choose time derivative state vector function
-accel_args = (w, interpolation_funcs)  # Passed to accel
+w, lookup_data = forces.load_essential_data(opt_gratings_data_fname, num_processes, output_opt_idx, lookup_data_fname)
+interpolation_funcs = forces.create_lsa_interpolation_funcs(lookup_data)
+accel = forces.aM_linear  # Choose time derivative state vector function
+accel_args = (w, interpolation_funcs, 1.)  # Passed to accel
 
 ## Optimisation parameters and initial conditions ##
 x0 = 0
 vx0 = 0
 
-## Optimised - 1st
+# # Standard perturbation
+# y0      = 0.05*w  # metres
+# phi0    = 1.*np.pi/180  # degrees converted to radians
+# vy0     = -1.  # metres per second
+# omega0  = -1.  # radians per second
+
+# Reduced perturbation
 y0      = 0.01*w  # metres
 phi0    = 0.1*np.pi/180  # degrees converted to radians
 vy0     = -0.1  # metres per second
 omega0  = -0.05*2*np.pi  # rotations per second converted to radians per second
-
-# # Observing divergences
-# x0 = 215177197.96531236
-# y0 = -0.09459471262152874
-# phi0 = -0.0027986233605225125
-# vx0 = 2658365.0469323145
-# vy0 = -11.247767769881074
-# omega0 = 0.9393570477723936
 
 ## Ilic - 1st
 # y0      = 1.3183489420398592e-07
@@ -63,21 +64,12 @@ omega0  = -0.05*2*np.pi  # rotations per second converted to radians per second
 # omega0  = -0.06679404481428496
 
 
-# x0     = 0
-# y0     = -0.05*L
-# phi0   = 0
-# vx0    = 0
-# vy0    = 0
-# omega0 = 0
-
 Y0 = np.array([x0,y0,phi0,vx0,vy0,omega0])
-
 time_MAX = 8.5*60*60  # Maximum runtime (seconds)
 # time_MAX = 10  # Maximum runtime (seconds)
 velocity_MAX = 0.05*c
- 
 h = 1e-4   # Step size  
-runID = "LvRFinalOpt"  # Added to the output data filename
+runID = "MdSnpminOpt_lsa"  # Added to the output data filename
 
 positions, angles, times, accels, loop_data = odecmvint(accel, Y0, time_MAX, velocity_MAX, args=accel_args, hstep=h)
 
