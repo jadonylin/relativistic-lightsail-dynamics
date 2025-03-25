8c93b32bbe7e288a26073ba3124e6298d9e5b83a
jadonylin <jadonlin2@gmail.com>
2025-03-25 11:05:33 +1100

    Add missing has_angle_data argument

--- Dynamics/Dynamics_integrator.py
+++ Dynamics/Dynamics_integrator.py
@@ -23,28 +23,26 @@ wavelength = 1
 # The efficiency factors are too expensive to calculate in real time, so pre-calculated tables are used.
 klambda = 1000
 kdelta = 1000
-runID_load = "MdSnpminOpt_lsa"
+runID_load = "MdSnpminOpt20_lsa"
 nonlinear_run = False  # Flag to load the nonlinear data and acceleration function
 
 if nonlinear_run:
     lookup_data_fname = f'./Data/{runID_load}_Lookup_table_lambda_{klambda}_by_delta_{kdelta}.pkl'
 else:
     lookup_data_fname = f'./Data/{runID_load}_Lookup_table_lambda_{klambda}.pkl'
-opt_gratings_data_fname = f'./Data/FOM_optimisation_maxfev9000'
+opt_gratings_data_fname = f'../Optimisation/Data/FOM_optimisation_maxfev9000'
 num_processes = 18  # number of processes used in the optimisation to produce opt_gratings_data_fname.pkl
 output_opt_idx = 0  # Index of the optimum grating to output, lower corresponding to larger FOM
 
 w, lookup_data = forces.load_essential_data(opt_gratings_data_fname, num_processes, output_opt_idx, lookup_data_fname)
-interpolation_funcs = forces.create_interpolation_funcs(lookup_data)
+interpolation_funcs = forces.create_interpolation_funcs(lookup_data, has_angle_data=nonlinear_run)
 
 if nonlinear_run:
     accel = forces.aM  # Choose time derivative state vector function
     accel_args = (w, interpolation_funcs)  # Passed to accel
 else:
     accel = forces.aM_linear  
-    accel_args = (w, interpolation_funcs, 0.)  
-    # accel = forces.aM_forces_linear
-    # accel_args = (w, interpolation_funcs)
+    accel_args = (w, interpolation_funcs, 1.)  
 
 ## Optimisation parameters and initial conditions ##
 x0 = 0
@@ -60,7 +58,7 @@ vx0 = 0
 y0      = 0.01*w  # metres
 phi0    = 0.1*np.pi/180  # degrees converted to radians
 vy0     = -0.1  # metres per second
-omega0  = -0.05*2*np.pi  # rotations per second converted to radians per second
+omega0  = -0.05*2*np.pi  # revolutions per second converted to radians per second
 
 ## Ilic - 1st
 # y0      = 1.3183489420398592e-07
@@ -75,11 +73,11 @@ omega0  = -0.05*2*np.pi  # rotations per second converted to radians per second
 
 
 Y0 = np.array([x0,y0,phi0,vx0,vy0,omega0])
-time_MAX = 8.5*60*60  # Maximum runtime (seconds)
-# time_MAX = 0.001  # Maximum runtime (seconds)
-velocity_MAX = 0.05*c
+time_MAX = 24.*60*60  # Maximum runtime (seconds)
+# time_MAX = 1  # Maximum runtime (seconds)
+velocity_MAX = 0.2*c
 h = 1e-4   # Step size  
-runID = "MdSnpminOpt_lsa_no_damping"  # Added to the output data filename
+runID = "MdSnpminOpt20_lsa"  # Added to the output data filename
 
 positions, angles, times, accels, loop_data = odecmvint(accel, Y0, time_MAX, velocity_MAX, args=accel_args, hstep=h)
 
