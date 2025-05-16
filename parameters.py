6b468fa8fcdf5d50eba4c46ca1208513c74834f9
jadonylin <jadonlin2@gmail.com>
2025-05-17 08:55:52 +1000

    Remove unused initial optimisation gratings

--- parameters.py
+++ parameters.py
@@ -134,6 +134,20 @@ param_bounds = [(pitch_min, pitch_max), (h1_min, h1_max),
                 (substrate_eps_min, substrate_eps_max)]
 
 
+# Initial grating for optimisation (doesn't influence the global optimisation, but must be specified)
+def average(x,y): return (x+y)/2
+grating_pitch   = average(pitch_min, pitch_max)
+grating_depth   = average(h1_min, h1_max)
+box1_width      = average(box_width_min, box_width_max)/2
+box2_width      = box1_width/2
+box_centre_dist = average(box_centre_dist_min, box_centre_dist_max)
+box1_eps        = average(box_eps_min, box_eps_max)
+box2_eps        = box1_eps
+gaussian_width  = average(gaussian_width_min, gaussian_width_max)
+substrate_depth = average(substrate_depth_min, substrate_depth_max)
+substrate_eps   = average(substrate_eps_min, substrate_eps_max)
+
+
 def Parameters():
     return I0, L, m, c
 
@@ -142,51 +156,6 @@ def opt_Parameters():
 
 def Bounds():
     return h1_min, h1_max, param_bounds
-
-
-#######################################################
-## Initial grating
-Start = "middle"
-
-if Start == "Ilic":
-    wavelength = 1.5/D1_ND(2/100)
-
-    grating_pitch = np.float64(1.8/wavelength)
-    grating_depth = np.float64(0.5/wavelength)
-    box1_width = np.float64(0.15*grating_pitch)
-    box2_width = np.float64(0.35*grating_pitch)
-    box_centre_dist = np.float64(0.60*grating_pitch)
-    box1_eps = np.float64(3.5**2)
-    box2_eps = np.float64(3.5**2)
-    gaussian_width = np.float64(2*L)
-    substrate_depth = np.float64(0.5/wavelength)
-    substrate_eps = np.float64(1.45**2)
-
-if Start == "other":
-    grating_pitch   = np.float64(1.3181953910424888)
-    grating_depth   = np.float64(0.9981573495961377)
-    box1_width      = np.float64(0.3925889472885004)
-    box2_width      = np.float64(0.10992496313652637)
-    box_centre_dist = np.float64(0.46325492497782883)
-    box1_eps        = np.float64(2.3183598380520047)
-    box2_eps        = np.float64(2.6699220192321977)
-    gaussian_width  = np.float64(25.03906252365165)
-    substrate_depth = np.float64(2.9915278797460836)
-    substrate_eps   = np.float64(2.3183620304237067)
-
-if Start == "middle":
-    def average(x,y): return (x+y)/2
-    grating_pitch   = average(pitch_min, pitch_max)
-    grating_depth   = average(h1_min, h1_max)
-    box1_width      = average(box_width_min, box_width_max)/2
-    box2_width      = box1_width/2
-    box_centre_dist = average(box_centre_dist_min, box_centre_dist_max)
-    box1_eps        = average(box_eps_min, box_eps_max)
-    box2_eps        = box1_eps
-    gaussian_width  = average(gaussian_width_min, gaussian_width_max)
-    substrate_depth = average(substrate_depth_min, substrate_depth_max)
-    substrate_eps   = average(substrate_eps_min, substrate_eps_max)
-
 
 def Initial_bigrating():
     return [grating_pitch, grating_depth, box1_width, box2_width, box_centre_dist, box1_eps, box2_eps, gaussian_width, substrate_depth, substrate_eps]
