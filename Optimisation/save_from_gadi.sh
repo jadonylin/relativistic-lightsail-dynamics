6cb3bf5289d2b51905fdbbbc234969e1a810e8f9
jadonylin <jadonlin2@gmail.com>
2025-06-04 17:09:09 +1000

    Semi-automate saving data from repo to OneDrive

--- Optimisation/save_from_gadi.sh
+++ Optimisation/save_from_gadi.sh
@@ -1,2 +1,3 @@
-runID=MdSnpmin20_fixed_gaussian_and_substrate
+runID=Fasymp20_mirror
 scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* ./Data/
+scp ./Data/${runID}* /Users/jlin0351/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/MdS\ npmin\ FoM/final_speed20/maxtime1440
