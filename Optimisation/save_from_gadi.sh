0826fc48dcd690f5735d43952ce8e2e57692cad6
jadonylin <jadonlin2@gmail.com>
2025-06-11 16:24:36 +1000

    Save optimisation results directly to OneDrive data folder

--- Optimisation/save_from_gadi.sh
+++ Optimisation/save_from_gadi.sh
@@ -1,3 +1,6 @@
-runID=Fasymp20_fixed_gaussian_near_cutoff
-saveDir=/Users/jlin0351/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed20/maxtime1440/cutoff/
-scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* ${saveDir}
+finalSpeed=20
+maxTime=1440
+runID=Fasymp20_cutoff
+saveDir=/Users/jlin0351/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
+mkdir "${saveDir}"
+scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* "${saveDir}"
