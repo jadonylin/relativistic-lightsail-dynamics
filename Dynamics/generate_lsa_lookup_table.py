71300b47bd8f10817393d84ab403252ad11f0c89
jadonylin <jadonlin2@gmail.com>
2025-07-24 13:34:52 +1000

    Observe grating dynamics for w = 20 m optimisation

--- Dynamics/generate_lsa_lookup_table.py
+++ Dynamics/generate_lsa_lookup_table.py
@@ -17,11 +17,11 @@ from twobox import TwoBox
 t_start = time.time()
 
 ## Initialise grating
-runID = "Fasymp20_gaussian100_50GW"
+runID = "Fasymp20_fixgaussian20_50GW"
 final_speed = 20.
 num_cores = 200
 maxtime = 1440
-output_opt_idx = 7
+output_opt_idx = 0
 
 common_path = user_home_path_full / "Library/CloudStorage/OneDrive-TheUniversityofSydney(Students)/Doppler Damping - Jadon Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon's results"
 custom_folder_path = f"Fasymp/final_speed{int(final_speed)}/maxtime{int(maxtime)}/{runID}"
--- Optimisation/save_from_gadi.sh
+++ Optimisation/save_from_gadi.sh
@@ -1,6 +1,6 @@
-finalSpeed=20
+finalSpeed=1
 maxTime=1440
-runID=Fasymp20_fixgaussian2_50GW
-saveDir=/Users/jlin0351/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
+runID=Fasymp1_fixpitch0.1deg
+saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
 mkdir "${saveDir}"
 scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* "${saveDir}"
