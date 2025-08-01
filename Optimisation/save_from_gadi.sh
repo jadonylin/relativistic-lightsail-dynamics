da94bde5e4cf1efc75c562c53fc76c5e30d0c6d8
jadonylin <jadonlin2@gmail.com>
2025-08-01 16:53:19 +1000

    Observe results for Fasymp1 with 20 m gaussian

--- Optimisation/save_from_gadi.sh
+++ Optimisation/save_from_gadi.sh
@@ -1,6 +1,6 @@
 finalSpeed=20
 maxTime=1440
-runID=Fasymp20_cutoffrange_50GW
-saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
+runID=Fasymp1_fixgaussian20_50GW
+saveDir=/Users/jlin0351/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
 mkdir "${saveDir}"
 scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* "${saveDir}"
