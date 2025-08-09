b324a7a4608ebd38f3567a5a44842e824f8f41f1
jadonylin <jadonlin2@gmail.com>
2025-08-09 10:48:54 +1000

    Analyse single-wavelength ng30 optimisation results

--- Optimisation/save_from_gadi.sh
+++ Optimisation/save_from_gadi.sh
@@ -1,6 +1,7 @@
 finalSpeed=20
 maxTime=1440
-runID=Fasympmonochrome_fixgaussian20_50GW
-saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
+runID=Fasympmonochrome_fixgaussian20_50GW_nG30
+#saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
+saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/mono/maxtime${maxTime}/${runID}
 mkdir "${saveDir}"
 scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* "${saveDir}"
