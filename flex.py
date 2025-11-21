369a05dce476c94c3e892f7d40c7ebf69079120b
jadonylin <jadonlin2@gmail.com>
2025-11-21 14:48:22 +1100

    Allow finite difference method to calculate elongation derivative

--- flex.py
+++ flex.py
@@ -44,20 +44,29 @@ def Qpr2_elongated(grating: TwoBox, scale: float=1.0) -> float:
     grating.box_centre_dist = bcd
     return Qpr2
 
-def dQpr2_dscale(grating: TwoBox, scale: float=1.0) -> float:
+def dQpr2_dscale(grating: TwoBox, scale: float=1.0, grad_method: str="finite") -> float:
     """
     Calculate the derivative of the radiation pressure efficiency Qpr2 
     with respect to elongation parallel to the grating.
 
     Parameters
     ----------
-    grating :   The TwoBox grating object.
-    scale   :   Scale factor for elongation, by default 1.0.
+    grating     :   The TwoBox grating object.
+    scale       :   Scale factor for elongation, by default 1.0.
+    grad_method :   Method for gradient calculation.
 
     Returns
     -------
     dQpr2_dscale : float
     """
     scale = grating.npa.array(scale)
-    grad_func = grating.npa.grad(Qpr2_elongated, argnum=1)
-    return grad_func(grating, scale)
+    if grad_method == "finite":
+        h = 1e-6
+        Qpr2_plus = Qpr2_elongated(grating, scale + h)
+        Qpr2_minus = Qpr2_elongated(grating, scale - h)
+        return (Qpr2_plus - Qpr2_minus)/(2*h)
+    elif grad_method == "grad":
+        grad_func = grating.npa.grad(Qpr2_elongated, argnum=1)
+        return grad_func(grating, scale)
+    else:
+        raise ValueError(f"Unknown grad_method: {grad_method}")
