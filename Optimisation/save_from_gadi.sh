4578a010298a82cc858ef3eae64b37d667e5389b
jadonylin <jadonlin2@gmail.com>
2026-03-16 14:43:20 +1100

    Delete unnecessary files for PRA upload

--- Optimisation/save_from_gadi.sh
+++
@@ -1,7 +0,0 @@
-finalSpeed=20
-maxTime=1440
-runID=Fkpr_unstablemonochrome_fixgaussian20_50GW
-#saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
-saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fkpr_unstable/mono/maxtime${maxTime}/${runID}
-mkdir "${saveDir}"
-scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* "${saveDir}"
--- Optimisation/transfer_to_gadi.sh
+++
@@ -1,3 +0,0 @@
-scp opt.py run_parallel.py run_optimisation.sh \
-    ../autolib.py ../flex.py ../fom.py ../parameters.py ../plotbox.py ../qprbox.py ../twobox.py \
-    jl7180@gadi.nci.org.au:~/RotationTwobox/
