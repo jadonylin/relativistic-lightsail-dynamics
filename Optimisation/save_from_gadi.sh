ff046e1ef4710b69b903a27abfd2a1b89bbdf634
jadonylin <jadonlin2@gmail.com>
2026-06-16 09:47:15 +1000

    Make data directories recursively to avoid manual creation

--- Optimisation/save_from_gadi.sh
+++ Optimisation/save_from_gadi.sh
@@ -1,7 +1,8 @@
-finalSpeed=20
+finalSpeed=2
 maxTime=1440
-runID=Fkpr_unstablemonochrome_fixgaussian20_50GW
-#saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
-saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fkpr_unstable/mono/maxtime${maxTime}/${runID}
-mkdir "${saveDir}"
+# runID=Fkpr_unstablemonochrome_fixgaussian20_50GW
+runID=Fasymp${finalSpeed}_fixgaussian20_50GW
+saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
+# saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fkpr_unstable/mono/maxtime${maxTime}/${runID}
+mkdir -p "${saveDir}"
 scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* "${saveDir}"
