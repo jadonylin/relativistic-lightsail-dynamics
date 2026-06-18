6b4cb49f49933a2527b1387ff627f519ff2f6ab6
jadonylin <jadonlin2@gmail.com>
2026-06-18 15:48:57 +1000

    Test range of stable initial conditions

--- Dynamics/Dynamics_integrator.py
+++ Dynamics/Dynamics_integrator.py
@@ -22,18 +22,18 @@ wavelength = 1
 
 # The efficiency factors are too expensive to calculate in real time, so pre-calculated tables are used.
 klambda = 1000
-kdelta = 1000
-runID_load = "Famp20_mirror"
-nonlinear_run = False  # Flag to load the nonlinear data and acceleration function
+kdelta = klambda
+runID_load = "Fasymp20_fixgaussian20_50GW"
+nonlinear_run = True  # Flag to load the nonlinear data and acceleration function
 damping_scaler = 1.  # Only applies if nonlinear_run is False. Set to 0 to disable damping.
 
 # Extract optimised grating (in particular, Gaussian width)
 num_processes = 200  # number of processes used in the optimisation to produce opt_gratings_data_fname.pkl
 maxtime = 1440
-output_opt_idx = 17  # Index of the optimum grating to output, lower corresponding to larger FOM
+output_opt_idx = 0  # Index of the optimum grating to output, lower corresponding to larger FOM
 
 if nonlinear_run:
-    lookup_data_fname = f'./Data/{runID_load}_Lookup_table_lambda_{klambda}_by_delta_{kdelta}.pkl'
+    lookup_data_fname = f'./Data/{runID_load}_Lookup_table_lambda_{klambda}_by_delta_{kdelta}_allangles.pkl'
 else:
     lookup_data_fname = f'./Data/{runID_load}_lsa_Lookup_table_lambda_{klambda}.pkl'
 
@@ -46,29 +46,23 @@ t0 = 0.
 x0 = 0
 vx0 = 0
 
-# # Reduced perturbation
-# y0      = 0.01*w  # metres
-# phi0    = 0.1*np.pi/180  # degrees converted to radians
-# vy0     = -0.1  # metres per second
-# omega0  = -0.05*2*np.pi  # revolutions per second converted to radians per second
-
-# # Minimal perturbation
+# # Moderate velocity
 # y0      = 0.001*w  # metres
 # phi0    = 0.01*np.pi/180  # degrees converted to radians
-# vy0     = -0.001  # metres per second
+# vy0     = -0.1  # metres per second
 # omega0  = -0.005*2*np.pi  # revolutions per second converted to radians per second
 
-# Moderate velocity
-y0      = 0.001*w  # metres
-phi0    = 0.01*np.pi/180  # degrees converted to radians
-vy0     = -0.1  # metres per second
-omega0  = -0.005*2*np.pi  # revolutions per second converted to radians per second
+# Custom
+y0      = 0.*w  # metres
+phi0    = 0.*np.pi/180  # degrees converted to radians
+vy0     = 0.  # metres per second
+omega0  = 1.*2*np.pi  # revolutions per second converted to radians per second
 
-time_MAX = 72.*60*60  # Maximum runtime (seconds)
-# time_MAX = 10  # Maximum runtime (seconds)
+# time_MAX = 72.*60*60  # Maximum runtime (seconds)
+time_MAX = 120  # Maximum runtime (seconds)
 velocity_MAX = 0.2*c
 h = 1e-3   # Step size  
-runID = "Fasymp20_gaussian100_50GW"  # For saving dynamics data
+runID = runID_load + f"_{omega0/(2*np.pi):.1f}rps"  # For saving dynamics data
 
 
 if nonlinear_run:
