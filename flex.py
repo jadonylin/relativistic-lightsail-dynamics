8316ea3439df5dc30d02b7d1427b75417d67aafa
jadonylin <jadonlin2@gmail.com>
2026-07-20 19:05:30 +1000

    Add temperature variable to grating strain
    Add functions to scale grating and calculate Jacobian matrix
    
    Rename scale to strain

--- flex.py
+++ flex.py
@@ -1,65 +1,98 @@
 """
-A module to store helpful functions and classes for plotting.
-
-In particular, the PlotBox class contains methods for plotting the permittivity profile, 
-angular efficiency, spectrum, and field distributions of a TwoBox grating.
+A module to store Qpr functions with thermal and elastic coupling.
 """
+import materials
 
-def Qpr_elongated(grating, scale: float=1.0) -> float:
+def grating_scaler(grating, strain: float=0., temp: float=0., material: dict=materials.Si3N4) -> float:
     """
-    Calculate the radiation pressure efficiency Qprj as a function of elongation
-    parallel to the grating.
+    Scale the grating due to longitudinal-wave strain and temperature change.
 
     Parameters
     ----------
-    grating :   The TwoBox grating object.
-    scale   :   Scale factor for elongation, by default 1.0.
+    grating  :   The TwoBox grating object.
+    strain   :   strain factor for elongation
+    temp     :   Temperature relative to rest temperature in Kelvin
+    material :   Grating material
+
+    Returns
+    -------
+    scaled_grating : grating with physical properties scaled
+    """
+
+    # Store original parameters
+    p = grating.grating_pitch
+    h = grating.grating_depth
+    w1 = grating.box1_width 
+    w2 = grating.box2_width 
+    bcd = grating.box_centre_dist 
+    hs = grating.substrate_depth    
+
+    nu = material["Poisson"]
+    alpha = material["thermal_expansion"]
+
+    scale_from_strain = 1 + strain
+    scale_from_temp = 1 + alpha*temp
+    grating.grating_pitch = p*scale_from_strain*scale_from_temp
+    grating.box1_width = w1*scale_from_strain*scale_from_temp
+    grating.box2_width = w2*scale_from_strain*scale_from_temp
+    grating.box_centre_dist = bcd*scale_from_strain*scale_from_temp
+    
+    zscale = 1 - nu*strain + (1+nu)*alpha*temp
+    grating.grating_depth = h*zscale
+    grating.substrate_depth = hs*zscale
+
+    if strain != 0. or temp != 0.:
+        raise ValueError("Warning: input grating parameters have been modified due to request for non-zero strain or temperature.")
+    return grating
+
+def jacobian_maker(func, grating, strain: float=0., temp: float=0., material: dict=materials.Si3N4) -> float:
+    """
+    Generates the jacobian for func with respect to strain and temperature-change variables.
+
+    Parameters
+    ----------
+    func     :   Function to calculate the jacobian for, with inputs (grating, strain, temp, material)
+    grating  :   The TwoBox grating object.
+    strain   :   strain factor for elongation
+    temp     :   Temperature relative to rest temperature in Kelvin
+    material :   Material properties dictionary
+
+    Returns
+    -------
+    |∂func/∂ϵ, ∂func/∂θ|
+    |∂func/∂ϵ, ∂func/∂θ|
+    """
+    strain = grating.npa.array(strain)
+    temp = grating.npa.array(temp)
+    def _func(p):
+        strain, temp = p
+        return func(grating, strain, temp, material)
+    p = grating.npa.array([strain, temp])
+    return grating.npa.array(grating.npa.jacobian(_func)(p).squeeze())
+
+
+def Qpr(grating, strain: float=0., temp: float=0., material: dict=materials.Si3N4) -> float:
+    """
+    Calculate the radiation pressure efficiency Qprj as a function of strain
+    parallel to the grating and temperature. Temperature changes are assumed
+    to linearly expand the grating in the directions parallel and perpendicular
+    to the grating.
+
+    Parameters
+    ----------
+    grating  :   The TwoBox grating object.
+    strain   :   strain factor for elongation
+    temp     :   Temperature relative to rest temperature in Kelvin
+    material :   Material properties dictionary
 
     Returns
     -------
     [Qpr1, Qpr2] : Radiation pressure efficiency factors
     """
+    _grating = grating_scaler(grating, strain, temp, material)
+    return _grating.Q()
 
-    # Store original parameters
-    p = grating.grating_pitch
-    w1 = grating.box1_width 
-    w2 = grating.box2_width 
-    bcd = grating.box_centre_dist 
-
-    grating.grating_pitch = p * scale
-    grating.box1_width = w1 * scale
-    grating.box2_width = w2 * scale
-    grating.box_centre_dist = bcd * scale
-    Qprs = grating.Q()
-
-    # TODO: Is there a better way to use the grating object without modifying the original?
-    # Restore original parameters
-    grating.grating_pitch = p
-    grating.box1_width = w1
-    grating.box2_width = w2
-    grating.box_centre_dist = bcd
-    return Qprs
-
-def dQpr_dscale(grating, scale: float=1.0) -> float:
-    """
-    Calculate the derivative of the radiation pressure efficiency Qprj 
-    with respect to elongation parallel to the grating.
-
-    Parameters
-    ----------
-    grating     :   The TwoBox grating object.
-    scale       :   Scale factor for elongation, by default 1.0.
-
-    Returns
-    -------
-    [dQpr1_dscale, dQpr2_dscale] : Scale derivatives of radiation pressure efficiency factors
-    """
-    scale = grating.npa.array(scale)
-    grad_func = grating.npa.grad(Qpr_elongated, argnum=1)
-    return grad_func(grating, scale)
-
-
-def Qprj_elongated(grating, j: int=2, scale: float=1.0) -> float:
+def Qprj(grating, j: int=2, strain: float=0.) -> float:
     """
     Calculate the radiation pressure efficiency Qprj as a function of elongation
     parallel to the grating.
@@ -68,7 +101,7 @@ def Qprj_elongated(grating, j: int=2, scale: float=1.0) -> float:
     ----------
     grating :   The TwoBox grating object.
     j       :   Index of the radiation pressure efficiency to calculate (1 for Qpr1, 2 for Qpr2).
-    scale   :   Scale factor for elongation, by default 1.0.
+    strain  :   strain factor for elongation
 
     Returns
     -------
@@ -77,120 +110,141 @@ def Qprj_elongated(grating, j: int=2, scale: float=1.0) -> float:
 
     if j not in [1, 2]:
         raise ValueError(f"Invalid value for j: {j}. Must be 1 or 2.")
-    return Qpr_elongated(grating, scale)[j-1]
+    return Qpr(grating, strain)[j-1]
 
-def dQprj_dscale(grating, j: int=2, scale: float=1.0, grad_method: str="finite") -> float:
+def dQpr(grating, strain: float=0., temp: float=0., material: dict=materials.Si3N4) -> float:
+    """
+    Calculate the derivative of the radiation pressure efficiency Qpr1 and Qpr2
+    with respect to strain and temperature change.
+
+    Parameters
+    ----------
+    grating  :   The TwoBox grating object.
+    strain   :   strain factor for elongation
+    temp     :   Temperature relative to rest temperature in Kelvin
+    material :   Material properties dictionary
+
+    Returns
+    -------
+    |∂Qpr1/∂ϵ, ∂Qpr1/∂θ|
+    |∂Qpr2/∂ϵ, ∂Qpr2/∂θ|
+    """
+    return jacobian_maker(Qpr, grating, strain, temp, material)
+
+def dQpr_dstrain(grating, strain: float=0., material: dict=materials.Si3N4) -> float:
     """
     Calculate the derivative of the radiation pressure efficiency Qprj 
-    with respect to elongation parallel to the grating.
+    with respect to strain parallel to the grating.
+
+    Parameters
+    ----------
+    grating     :   The TwoBox grating object.
+    strain      :   strain factor for elongation
+    material    :   Material properties dictionary
+
+    Returns
+    -------
+    [dQpr1_dstrain, dQpr2_dstrain] : strain derivatives of radiation pressure efficiency factors
+    """
+    return dQpr(grating, strain, 0., material)[:,0]
+
+def dQprj_dstrain(grating, j: int=2, strain: float=0., grad_method: str="finite") -> float:
+    """
+    Calculate the derivative of the radiation pressure efficiency Qprj 
+    with respect to strain parallel to the grating.
 
     Parameters
     ----------
     grating     :   The TwoBox grating object.
     j           :   Index of the radiation pressure efficiency to calculate (1 for Qpr1, 2 for Qpr2).
-    scale       :   Scale factor for elongation, by default 1.0.
+    strain      :   strain factor for elongation
     grad_method :   Method for gradient calculation.
 
     Returns
     -------
-    dQprj_dscale : float
+    dQprj_dstrain : float
     """
     if j not in [1, 2]:
         raise ValueError(f"Invalid value for j: {j}. Must be 1 or 2.")
     if grad_method == "finite":
         h = 1e-6
-        Qprj_plus = Qprj_elongated(grating, j, scale + h)
-        Qprj_minus = Qprj_elongated(grating, j, scale - h)
+        Qprj_plus = Qprj(grating, j, strain + h)
+        Qprj_minus = Qprj(grating, j, strain - h)
         return (Qprj_plus - Qprj_minus)/(2*h)
     elif grad_method == "grad":
-        return dQpr_dscale(grating, scale)[j-1]
+        return dQpr_dstrain(grating, strain)[j-1]
     else:
         raise ValueError(f"Unknown grad_method: {grad_method}")
 
-def d2Qprj_dscale2(grating, j: int=2, scale: float=1.0, grad_method: str="finite") -> float:
+def d2Qprj_dstrain2(grating, j: int=2, strain: float=0., grad_method: str="finite") -> float:
     """
     Calculate the second derivative of the radiation pressure efficiency Qprj 
-    with respect to elongation parallel to the grating.
+    with respect to strain parallel to the grating.
+    TODO: NOT UPDATED
 
     Parameters
     ----------
     grating     :   The TwoBox grating object.
     j           :   Index of the radiation pressure efficiency to calculate (1 for Qpr1, 2 for Qpr2).
-    scale       :   Scale factor for elongation, by default 1.0.
+    strain      :   strain factor for elongation
     grad_method :   Method for gradient calculation.
 
     Returns
     -------
-    dQprj_dscale : float
+    dQprj_dstrain : float
     """
     if j not in [1, 2]:
         raise ValueError(f"Invalid value for j: {j}. Must be 1 or 2.")
     if grad_method == "finite":
         h = 1e-6
-        # Qprj_plus = dQprj_dscale(grating, j, scale + h, grad_method="grad")
-        # Qprj_minus = dQprj_dscale(grating, j, scale - h, grad_method="grad")
-        Qprj_plus = dQprj_dscale(grating, j, scale + h, grad_method="finite")
-        Qprj_minus = dQprj_dscale(grating, j, scale - h, grad_method="finite")
+        # Qprj_plus = dQprj_dstrain(grating, j, strain + h, grad_method="grad")
+        # Qprj_minus = dQprj_dstrain(grating, j, strain - h, grad_method="grad")
+        Qprj_plus = dQprj_dstrain(grating, j, strain + h, grad_method="finite")
+        Qprj_minus = dQprj_dstrain(grating, j, strain - h, grad_method="finite")
         return (Qprj_plus - Qprj_minus)/(2*h)
     elif grad_method == "grad":
-        scale = grating.npa.array(scale)
-        func = lambda s: dQprj_dscale(grating, j, s, grad_method="grad")
+        strain = grating.npa.array(strain)
+        func = lambda s: dQprj_dstrain(grating, j, s, grad_method="grad")
         grad_func = grating.npa.grad(func)
-        return grad_func(scale)
+        return grad_func(strain)
     else:
         raise ValueError(f"Unknown grad_method: {grad_method}")
     
 
-def absorption(grating, scale: float=1.0) -> float:
+def absorption(grating, strain: float=0., temp: float=0., material: dict=materials.Si3N4) -> float:
     """
     Calculate the absorption (1-R-T) as a function of elongation
     parallel to the grating.
 
     Parameters
     ----------
-    grating :   The TwoBox grating object.
-    scale   :   Scale factor for elongation, by default 1.0.
+    grating  :   The TwoBox grating object.
+    strain   :   strain factor for elongation
+    temp     :   Temperature relative to rest temperature in Kelvin
+    material :   Material properties dictionary
 
     Returns
     -------
     absorption : absorption of the grating
     """
-
-    # Store original parameters
-    p = grating.grating_pitch
-    w1 = grating.box1_width 
-    w2 = grating.box2_width 
-    bcd = grating.box_centre_dist 
-
-    grating.grating_pitch = p * scale
-    grating.box1_width = w1 * scale
-    grating.box2_width = w2 * scale
-    grating.box_centre_dist = bcd * scale
-    Rs, Ts = grating.eff()
-    absorption = 1 - grating.npa.sum(Rs + Ts)
-
-    # TODO: Is there a better way to use the grating object without modifying the original?
-    # Restore original parameters
-    grating.grating_pitch = p
-    grating.box1_width = w1
-    grating.box2_width = w2
-    grating.box_centre_dist = bcd
+    _grating = grating_scaler(grating, strain, temp, material)
+    Rs, Ts = _grating.eff()
+    absorption = 1 - _grating.npa.sum(Rs + Ts)
     return absorption
 
-def dabsorption_dscale(grating, scale: float=1.0) -> float:
+def dabsorption(grating, strain: float=0., temp: float=0., material: dict=materials.Si3N4) -> float:
     """
-    Calculate the derivative of the absorption
-    with respect to elongation parallel to the grating.
+    Calculate the derivative of the absorption with respect to strain and temperature change.
 
     Parameters
     ----------
-    grating     :   The TwoBox grating object.
-    scale       :   Scale factor for elongation, by default 1.0.
+    grating  :   The TwoBox grating object.
+    strain   :   strain factor for elongation
+    temp     :   Temperature relative to rest temperature in Kelvin
+    material :   Material properties dictionary
 
     Returns
     -------
-    dabsorption_dscale : Scale derivative of absorption
+    [∂a/∂ϵ, ∂a/∂θ]
     """
-    scale = grating.npa.array(scale)
-    grad_func = grating.npa.grad(absorption, argnum=1)
-    return grad_func(grating, scale)
+    return jacobian_maker(absorption, grating, strain, temp, material)
