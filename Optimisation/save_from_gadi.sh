caddc77cc3afa285bc2b2bb660d49f6100df928e
jadonylin <jadonlin2@gmail.com>
2026-08-06 16:18:25 +1000

    Save optoelastic_regime results from Gadi

--- Optimisation/save_from_gadi.sh
+++ Optimisation/save_from_gadi.sh
@@ -1,8 +1,10 @@
-finalSpeed=2
+finalSpeed=20
 maxTime=1440
+runID=Foptoelastic_regimemonochrome_LO_MMA_fixgaussian20_50GW
 # runID=Fkpr_unstablemonochrome_fixgaussian20_50GW
-runID=Fasymp${finalSpeed}_fixgaussian20_50GW
-saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
+# runID=Fasymp${finalSpeed}_fixgaussian20_50GW
+# saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fasymp/final_speed${finalSpeed}/maxtime${maxTime}/${runID}
 # saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Fkpr_unstable/mono/maxtime${maxTime}/${runID}
+saveDir=/Users/jadonlin/Library/CloudStorage/OneDrive-TheUniversityofSydney\(Students\)/Doppler\ Damping\ -\ Jadon\ Lin/Documentation/Data/relativistic-lightsail-dynamics/Optimisation/Jadon\'s\ results/Foptoelastic_regime/mono/maxtime${maxTime}/${runID}
 mkdir -p "${saveDir}"
 scp jl7180@gadi-dm.nci.org.au:~/RotationTwobox/Data/${runID}* "${saveDir}"
